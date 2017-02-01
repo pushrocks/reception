0538298af728b70f4f5a9378494ef802440b9d5e
Phil Kunz <gitlab@philkunz.com>
2017-02-02 00:06:18 +0100

    compile

--- dist/reception.classes.reception.d.ts
+++ dist/reception.classes.reception.d.ts
@@ -2,13 +2,31 @@
 import * as plugins from './reception.plugins';
 import { Express as IExpress } from 'express';
 import { Deferred as ICDeferred } from 'smartq';
+export declare type TStrategy = 'Google' | 'Facebook' | 'Twitter';
+export interface IStrategyData {
+    Twitter?: {
+        consumerKey: string;
+        consumerSecret: string;
+    };
+    Facebook?: {
+        clientID: string;
+        clientSecret: string;
+    };
+    Google?: {
+        clientID: string;
+        clientSecret: string;
+    };
+}
+export interface IReceptionConstructorOptions {
+    strategyData: IStrategyData;
+}
 export declare class Reception {
     expressApp: IExpress;
     expressPort: number;
     ppStrategyFacebook: plugins.passportFacebook.Strategy;
     ppStategyTwitter: plugins.passportTwitter.Strategy;
     ppStrategyGoogle: plugins.passportGoogle.OAuth2Strategy;
-    protected runningDeferred: ICDeferred<number>;
     running: Promise<number>;
-    constructor();
+    protected runningDeferred: ICDeferred<number>;
+    constructor(optionsArg: IReceptionConstructorOptions);
 }
